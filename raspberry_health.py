d3bb7f76a22dcb86a6725c5913cfaf02a646dfc4
derHeinz <heinzserfas@gmx.de>
2018-05-10 17:43:40 +0200

    fixed subprocess, fixed exit behavior

--- raspberry_health.py
+++ raspberry_health.py
@@ -3,7 +3,10 @@
 
 import logging
 import platform
-from subprocess import PIPE, Popen
+import signal
+import time
+import sys
+import os
 from network_api import NetworkAPI
 
 class RaspberryHealth(object):
@@ -11,6 +14,8 @@ class RaspberryHealth(object):
 	def __init__(self):
 		super(RaspberryHealth, self).__init__()
 		self._api = NetworkAPI(self)
+	
+	def start(self):
 		self._api.start()
 		
 class RaspberryHealthWindows(RaspberryHealth):
@@ -31,16 +36,26 @@ class RaspberryHealthLinux(RaspberryHealth):
 
 	def cpu_temp(self):
 		"""get cpu temperature using vcgencmd"""
-		process = subprocess.Popen(['vcgencmd', 'measure_temp'], stdout=subprocess.PIPE)
+		process = self.subprocess.Popen(['vcgencmd', 'measure_temp'], stdout=self.subprocess.PIPE)
 		output, _error = process.communicate()
 		return float(output[output.index('=') + 1:output.rindex("'")])
 		
 	def cpu_clock(self):
 		return "500"
 		
+def signal_handler(signal, frame):
+	print('Exiting!')
+	os._exit(0)
+
 if __name__ == '__main__':
 	pf = platform.system()
+	print("Starting \nPress CTRL-C to exit")
+	ph = None
 	if "Windows" == pf:
-		RaspberryHealthWindows()
+		ph = RaspberryHealthWindows()
 	else:
-		RaspberryHealthLinux()
+		ph = RaspberryHealthLinux()
+	ph.start()
+	signal.signal(signal.SIGINT, signal_handler)
+	while True:
+		time.sleep(1000)
